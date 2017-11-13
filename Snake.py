c5aed7c1d04a390a3eec82be27410646ef73085d
Adwait Rawat <31562427+adwaitrawat@users.noreply.github.com>
2017-11-13 21:01:01 +0530

    Add files via upload
    some changes in the main project file and a new file with incomplete but working functionalities which are in the main file and

--- Snake.py
+++ Snake.py
@@ -1,18 +1,18 @@
 import random, pygame 
-pygame.init()
 
 size = width, height = 1000, 1000
 pixel = 5
 speed = [pixel,0]
 head = [500,400]
-food = []
-eaten = 0
-WHITE = (0,0,0)
+food = [0,0]
+eaten = [0]
+WHITE = (255,255,255)
 RED = (255,0,0)
-GREEN = (0,0,255)
-BLUE = (0,255,0)
+GREEN = (0,255,0)
+BLUE = (0,0,255)
 YELLOW = (0,255,255)
-BLACK = (255,255,255)
+BLACK = (0,0,0)
+fps=15
 
 class tail(object):
 
@@ -49,14 +49,18 @@ class tail(object):
 
 tails = [tail(490,400,pixel,0)]
 
-def move(self):
+def move():
 	for i in range (0,2):
+		display()
 		head[i]+=speed[i]
+		pygame.draw.rect(screen, GREEN, (head[0]-pixel,head[1]-pixel,2*pixel,2*pixel), 0)
+		pygame.display.update()
+		print(head)
 	pass
 
 
 def wall():
-	if head[0] + pixel*2 == 1000 or head[1] + pixel*2 == 800 :
+	if head[0] + pixel >= 1000 or head[1] + pixel >= 800 or head[0]  <= pixel*2 or head[1] + pixel <= pixel*2 :
 		return 0
 	else:
 		return 1
@@ -64,52 +68,54 @@ def wall():
 def speed_update():
 	for i in range(0,2): 
 		if not speed[i] == 0 :
-			speed[i]+=speed[i]
+			speed[i]+=0.1*eaten[0]
 	pass
 
 def eat():
-	if head == food :
+	if head[0] <= food[0] + pixel and head[1] <= food[1] + pixel and head[0] >= food[0] - pixel and head[1] >= food[1] - pixel:
 		new_food()
-		eaten+=1
+		eaten[0]+=1
+		print(eaten)
+		speed_update()
 		return True
 	else :
 		return False
 
 def new_food():
+	display()
 	food[0]=random.randint(0,1000-pixel)
 	food[1]=random.randint(0,800-pixel)
 	pygame.draw.circle(screen, YELLOW, (food[0], food[1]), pixel)
+	pygame.display.update()
 	pass
 
-def esc():
-	if (pygame.key.get_pressed()[[pygame.K_ESCAPE] != 0 ] ):
-		return 0
-	else : 
-		return 1
-
 def direction():
-	if pygame.key.get_pressed(K_UP) == True and speed[1] == 0 :
-		temp = speed[0]
-		speed[0] = 0
-		speed[1] = temp
-	elif pygame.key.get_pressed(K_DOWN) == True and speed[1] == 0 :
-		temp = -1*speed[0]
-		speed[0] = 0
-		speed[1] = temp
-	elif pygame.key.get_pressed(K_RIGHT) == True and speed[0] == 0 :
-		temp = speed[0]
-		speed[0] = 0
-		speed[1] = temp
-	elif pygame.key.get_pressed(K_LEFT) == True and speed[0] == 0:
-		temp = -1*speed[0]
-		speed[0] = 0
-		speed[1] = temp
+	if event.type == pygame.KEYDOWN :
+		if event.key == pygame.K_DOWN and speed[1] == 0 :
+			temp = speed[0]
+			speed[0] = 0
+			speed[1] = temp
+		if event.key == pygame.K_UP and speed[1] == 0 :
+			temp = speed[0]
+			speed[0] = 0
+			speed[1] = -1*temp
+		if event.key == pygame.K_RIGHT and speed[0] == 0 :
+			temp = speed[1]
+			speed[1] = 0
+			speed[0] = temp
+		if event.key == pygame.K_LEFT and speed[0] == 0:
+			temp = speed[1]
+			speed[1] = 0
+			speed[0] = -1*temp
+		if event.key == pygame.K_ESCAPE:
+			pygame.quit()
 	pass
 
 def display():
-	screen = pygame.display.set_mode(size)
 	screen.fill(BLACK)
-	pygame.draw.rect(screen, WHITE, [0,0,1000,800], pixel)
+	pygame.draw.rect(screen, WHITE, (0,0,1000,800), pixel)
+	pygame.draw.circle(screen, YELLOW, (food[0], food[1]), pixel)
+	pygame.display.update()
 
 def score():
 	#score board
@@ -130,21 +136,23 @@ def tail_cut():
 
 #--------------------------------------------------------------------------------------
 #main body
+screen = pygame.display.set_mode(size)
 clock = pygame.time.Clock()
+pygame.init()
 display()
-while wall() and esc() and tail_cut():
+new_food()
+move()
+while wall() and tail_cut():
 	clock.tick(15)
 	direction()
 	move()
 	first_tail()
 	for i in range(0,len(tails)-1) :
 		tails[i+1].position(i)
-	if eat() :
-		tails.append(tails[end].tail[0],tails[end].tail[1],tails[end].speed_tail[0],tails[end].speed_tail[1])
-	if eaten%3==0 :
-		speed_update()
+	#if eat() :
+	#	tails.append(tails[end].tail[0],tails[end].tail[1],tails[end].speed_tail[0],tails[end].speed_tail[1])
+
 
 #after exiting the game
 #graphically show that the player lost
 
-pygame.quit()
---
+++ incomplete patch.py
@@ -0,0 +1,97 @@
+import pygame, random ,threading
+
+size = width, height = 1000, 1000
+pixel = 5
+speed = [pixel,0]
+head = [500,400]
+food = [0,0]
+eaten = [0]
+WHITE = (255,255,255)
+RED = (255,0,0)
+GREEN = (0,255,0)
+BLUE = (0,0,255)
+YELLOW = (0,255,255)
+BLACK = (0,0,0)
+fps=15
+
+def direction():
+	if event.type == pygame.KEYDOWN :
+		if event.key == pygame.K_DOWN and speed[1] == 0 :
+			temp = speed[0]
+			speed[0] = 0
+			speed[1] = temp
+		if event.key == pygame.K_UP and speed[1] == 0 :
+			temp = speed[0]
+			speed[0] = 0
+			speed[1] = -1*temp
+		if event.key == pygame.K_RIGHT and speed[0] == 0 :
+			temp = speed[1]
+			speed[1] = 0
+			speed[0] = temp
+		if event.key == pygame.K_LEFT and speed[0] == 0:
+			temp = speed[1]
+			speed[1] = 0
+			speed[0] = -1*temp
+		if event.key == pygame.K_ESCAPE:
+			pygame.quit()
+	pass
+
+def display():
+	screen.fill(BLACK)
+	pygame.draw.rect(screen, WHITE, (0,0,1000,800), pixel)
+	pygame.draw.circle(screen, YELLOW, (food[0], food[1]), pixel)
+	pygame.display.update()
+
+def move():
+	for i in range (0,2):
+		display()
+		head[i]+=speed[i]
+		pygame.draw.rect(screen, GREEN, (head[0]-pixel,head[1]-pixel,2*pixel,2*pixel), 0)
+		pygame.display.update()
+		print(head)
+	pass
+
+
+def wall():
+	if head[0] + pixel >= 1000 or head[1] + pixel >= 800 or head[0]  <= pixel*2 or head[1] + pixel <= pixel*2 :
+		return 0
+	else:
+		return 1
+
+def speed_update():
+	for i in range(0,2): 
+		if not speed[i] == 0 :
+			speed[i]+=0.1*eaten[0]
+	pass
+
+def eat():
+	if head[0] <= food[0] + pixel and head[1] <= food[1] + pixel and head[0] >= food[0] - pixel and head[1] >= food[1] - pixel:
+		new_food()
+		eaten[0]+=1
+		print(eaten)
+		speed_update()
+		return True
+	else :
+		return False
+
+def new_food():
+	display()
+	food[0]=random.randint(0,1000-pixel)
+	food[1]=random.randint(0,800-pixel)
+	pygame.draw.circle(screen, YELLOW, (food[0], food[1]), pixel)
+	pygame.display.update()
+	pass
+
+screen = pygame.display.set_mode(size)
+clock = pygame.time.Clock()
+pygame.init()
+display()
+new_food()
+move()
+while wall() :
+	for event in pygame.event.get():
+		clock.tick(fps)
+		threading.Thread(target=move).start()
+		threading.Thread(target=direction).start()
+		threading.Thread(target=eat).start()
+		
